d01de9bd547da741cc76e7a5e8cf8d36524cade4
ccp84 <ccp84@icloud.com>
2022-12-06 21:33:28 +0000

    completed lesson

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,23 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log(sum);
 
 // Extra arguments are ignored
+const sumIgnore = (a, b, c) => a + b + c;
+sum = sumIgnore(1, 2, 3, 4, 5, 6);
+console.log(sum);
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum2 = a + b + c;
+    for (i of rest) {
+        sum2 += i;
+    }
+    return sum2;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log(sum3);
